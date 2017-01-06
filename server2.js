73ad461bd490ece7a43cd1bf433b00d9405f4b17
gareth <gmccumskey@gmail.com>
2017-01-06 15:11:41 +0200

    Changed port

--- server2.js
+++ server2.js
@@ -1,23 +1,17 @@
 var express = require('express');
 var app = express();
-var PORT = 3000;
+var PORT = process.env.port || 3000;
 
-var middleware = {
-    requireAuthentication: function (request, response, next) {
-        console.log('Private route hit');
-        next();
-    },
-    logger: function (request, response, next) {
-        console.log(new Date().toString() + " Request: " +request.method+ " " + request.originalUrl);
-        next();
-    }
-};
+var middleware = require('./middleware.js');
+
 app.use(middleware.logger);
-// app.use(middleware.requireAuthentication)
-app.get('/about', middleware.requireAuthentication, function (request, response) {
-    response.send('About us!');
+
+app.get('/about', middleware.requireAuthentication, function (req, res) {
+    res.send('About Us!');
 });
-app.use(express.static(__dirname+"/public"));
+
+app.use(express.static(__dirname + '/public'));
+
 app.listen(PORT, function () {
-    console.log('Express server started on port '+PORT);
+    console.log('Express server started on port ' + PORT + '!');
 });
